d53888089f42d35e8d8fba14aa7a37af1313a046
Connor Blake <cblake506@gmail.com>
2022-04-02 16:34:12 -0500

    Added password generation logic

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
 "use strict";
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword(length, restrictions){
+function generatePassword(passLength, restrictions){
   let lower = "abcdefghijklmnop";
   let upper = "ABCDEFGHIJKLMNOP";
   let numbers = "123456789";
   let special = "~`!@#$%^&*()_+[{]}\\|;:\'\",<.>/?";
-
+  let pool = [lower, upper, numbers, special];
   
+  // create an ordered string with numbers from each eligible pool
+  let orderedPass = "";
+  let count = 0;
+  let restrictionsIterator = 0;
+  while(orderedPass.length < passLength){
+    // evenly looping through only the user selected characters
+    restrictionsIterator = count % restrictions.length;
+    if(restrictions[restrictionsIterator]){
+      // picking a random index
+      let randIndex = Math.floor(Math.random() * pool[restrictionsIterator].length);
+      // appending the next character
+      orderedPass += pool[restrictionsIterator].charAt(randIndex);
+      count++;
+    }
+  }
+
   let password = "";
+  // shuffle the ordered password
+  for(let i = 0; i < passLength; i++){
+    let randIndex = Math.floor(Math.random() * orderedPass.length);
+    password += orderedPass.charAt(randIndex);
+    orderedPass = orderedPass.slice(0, randIndex) + orderedPass.slice(randIndex + 1);
+  }
+
   return password;
 }
 
@@ -37,12 +60,6 @@ function writePassword() {
   // prompt user and cast the input value as a number
   let passLength = promptLength();
   let restrictions = promptRestrictions();
-
-  // if the input value isn't a number or within 8-128 then re-prompt the user
-  while(!Number.isInteger(passLength) || passLength < 8 || passLength > 128){
-    passLength = Number(window.prompt("Error. Please choose password length from 8 to 128:"));
-  }
-
 
   var password = generatePassword(passLength, restrictions);
   var passwordText = document.querySelector("#password");
